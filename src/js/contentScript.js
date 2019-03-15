ac2fe794be965de6cadde1a8a29a23a09abdb8cd
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2019-03-15 09:03:56 -0700

    Use chrome.runtime instead of extensions

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,6 +1,6 @@
 // Loads contentMain.js which loads all other js files
 (async () => {
-  const src = chrome.extension.getURL('src/js/contentMain.js');
+  const src = chrome.runtime.getURL('src/js/contentMain.js');
   const contentScript = await import(src);
   contentScript.main();
 })();
